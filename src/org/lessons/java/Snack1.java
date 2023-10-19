5c30182bafa6c0acc5fa6f171597807a129df059
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-10-19 14:17:43 +0200

    Snack1

---
+++ src/org/lessons/java/Snack1.java
@@ -0,0 +1,20 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Snack1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Inserisci un numero: ");
+        int number = scan.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.print("Il tuo numero pari è: " + number);
+        } else if (number % 2 == 1) {
+            System.out.print("Hai inserito un numero dispari, stampo il numero successivo: " + (number + 1));
+        } else {
+            System.out.print("Hai inserito un numero negativo");
+        }
+    }
+}
